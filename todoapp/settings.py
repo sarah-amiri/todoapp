1a8a3684e6b2d257c1c037753667b8d3e6a30b41
Sarah Amiri <sarah.amiri28@gmail.com>
2020-12-06 10:04:38 +0330

    Add User model

--- todoapp/settings.py
+++ todoapp/settings.py
@@ -129,3 +129,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+
+
+# user authentication
+
+AUTH_USER_MODEL = 'user.User'
--- user/models.py
+++ user/models.py
@@ -1,3 +1,31 @@
+from django.contrib.auth.models import PermissionsMixin
+from django.contrib.auth.base_user import AbstractBaseUser, BaseUserManager
 from django.db import models
+from django.utils.translation import gettext_lazy as _
 
-# Create your models here.
+
+class User(AbstractBaseUser, PermissionsMixin):
+    """
+    Model to define my User model.
+    It is based on django AbstractBaseUser and uses PermissionsMixin.
+    It inherits `password` and `last_login` from AbstractBaseUser and
+    `is_superuser`, `groups` and `user_permissions` from PermissionsMixin.
+    """
+    email = models.EmailField(_('email address'), unique=True)
+    name = models.CharField(_('name'), max_length=100)
+    date_joined = models.DateTimeField(_('date joined'), auto_now_add=True)
+    is_active = models.BooleanField(default=True)
+
+    objects = BaseUserManager()
+
+    USERNAME_FIELD = 'email'
+
+    class Meta:
+        db_table = 'users'
+
+    def save(self, *args, **kwargs):
+        if not self.password:
+            raise ValueError('password must be set')
+
+        self.set_password(self.password)
+        return super().save(*args, **kwargs)
