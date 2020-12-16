370e612045b1ca3797aff398657074a0a93b7c99
Sarah Amiri <sarah.amiri28@gmail.com>
2020-12-16 12:19:27 +0330

    Add jwt_token APIs

--- user/urls.py
+++ user/urls.py
@@ -1,9 +1,13 @@
 from django.urls import path
 
+from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
+
 from .views import UserCreateAPIView, UserRetrieveUpdateAPIView
 
 
 urlpatterns = [
     path('register/', UserCreateAPIView.as_view(), name='register-user'),
-    path('<int:pk>/', UserRetrieveUpdateAPIView.as_view(), name='get-update-user')
+    path('<int:pk>/', UserRetrieveUpdateAPIView.as_view(), name='get-update-user'),
+    path('obtain_token/', TokenObtainPairView.as_view(), name='obtain_token'),
+    path('refresh_token/', TokenRefreshView.as_view(), name='refresh_token')
 ]
