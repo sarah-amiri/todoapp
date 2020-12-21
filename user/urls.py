5abd9036d80b8ab8e4a2db7b29dd4fcc9a5ab8ad
Sarah Amiri <sarah.amiri28@gmail.com>
2020-12-21 10:36:57 +0330

    Delete LogoutView from imports

--- user/urls.py
+++ user/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 
 from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 
-from .views import UserCreateAPIView, UserRetrieveUpdateAPIView, LogoutView
+from .views import UserCreateAPIView, UserRetrieveUpdateAPIView
 
 
 urlpatterns = [
