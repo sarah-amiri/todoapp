2e9387d61213f98b2273b515bd073f289d06aa2d
Sarah Amiri <sarah.amiri28@gmail.com>
2020-12-21 09:50:37 +0330

    Enable user get/update with jwt

--- user/urls.py
+++ user/urls.py
@@ -2,12 +2,12 @@ from django.urls import path
 
 from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 
-from .views import UserCreateAPIView, UserRetrieveUpdateAPIView
+from .views import UserCreateAPIView, UserRetrieveUpdateAPIView, LogoutView
 
 
 urlpatterns = [
+    path('', UserRetrieveUpdateAPIView.as_view(), name='get-update-user'),
     path('register/', UserCreateAPIView.as_view(), name='register-user'),
-    path('<int:pk>/', UserRetrieveUpdateAPIView.as_view(), name='get-update-user'),
     path('obtain_token/', TokenObtainPairView.as_view(), name='obtain_token'),
     path('refresh_token/', TokenRefreshView.as_view(), name='refresh_token')
 ]
--- user/views.py
+++ user/views.py
@@ -1,5 +1,10 @@
 from django.contrib.auth import get_user_model
-from rest_framework.generics import CreateAPIView, RetrieveUpdateAPIView
+from django.shortcuts import get_object_or_404
+
+from rest_framework.generics import CreateAPIView, RetrieveUpdateAPIView, GenericAPIView
+from rest_framework.permissions import AllowAny
+from rest_framework.views import APIView
+from rest_framework.response import Response
 
 from .serializers import UserSerializer
 
@@ -7,6 +12,7 @@ User = get_user_model()
 
 
 class UserCreateAPIView(CreateAPIView):
+    permission_classes = [AllowAny,]
     serializer_class = UserSerializer
     queryset = User.objects.all()
 
@@ -14,3 +20,21 @@ class UserCreateAPIView(CreateAPIView):
 class UserRetrieveUpdateAPIView(RetrieveUpdateAPIView):
     serializer_class = UserSerializer
     queryset = User.objects.all()
+
+    def get_object(self):
+        """
+        Return the object that view is going to display.
+        It is GenericAPIView function that override here.
+        """
+        queryset = self.filter_queryset(self.get_queryset())
+
+        # get object based on user id (identified by token)
+        filter_kwargs = {self.lookup_field: self.request.user.id}
+
+        obj = get_object_or_404(queryset, **filter_kwargs)
+
+        # check permissions for user
+        # here permission is IsAuthenticated but there may be other permissions
+        self.check_object_permissions(self.request, obj)
+
+        return obj
